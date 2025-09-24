b9410d3aacb8b4b014b3bc79366ff12f41f729f2
Akhil Dhiman <akhildhiman1897@gmail.com>
2025-09-24 23:15:49 +0530

    add the user input in js

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ const ans2 = sumToN(100);
 console.log(ans2);
 
 
+// take the input from the user for 1 to n sum
+// In Node.js
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter a number: ", function (n) {
+    n = parseInt(n);
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    console.log(`Sum from 1 to ${n} is: ${sum}`);
+    rl.close();
+});
+
+//
+
+
